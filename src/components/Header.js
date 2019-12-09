bdb1264c655fbe74ae929c47be15661f0bd1ca46
aramirezdecruz3148 <leigh3148@gmail.com>
2019-12-09 10:20:36 -0800

    added search navigation to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,25 +7,29 @@ class Header extends Component {
   render() {
     const authToken = localStorage.getItem(AUTH_TOKEN)
     return (
-      <div className="flex pa1 justify-between nowrap orange">
-        <div className="flex flex-fixed black">
-          <div className="fw7 mr1">Hacker News</div>
-          <Link to="/" className="ml1 no-underline black">
-            new
+      <div className='flex pa1 justify-between nowrap orange'>
+        <div className='flex flex-fixed black'>
+          <div className='fw7 mr1'>Hacker News</div>
+          <Link to='/' className='ml1 no-underline black'>
+            links
+          </Link>
+          <div className='ml1'>|</div>
+          <Link to='/search' className='ml1 no-underline black'>
+            search
           </Link>
           {authToken && (
-            <div className="flex">
-              <div className="ml1">|</div>
-              <Link to="/create" className="ml1 no-underline black">
+            <div className='flex'>
+              <div className='ml1'>|</div>
+              <Link to='/create' className='ml1 no-underline black'>
                 submit
               </Link>
             </div>
           )}
         </div>
-        <div className="flex flex-fixed">
+        <div className='flex flex-fixed'>
           {authToken ? (
             <div
-              className="ml1 pointer black"
+              className='ml1 pointer black'
               onClick={() => {
                 localStorage.removeItem(AUTH_TOKEN)
                 this.props.history.push(`/`)
@@ -34,7 +38,7 @@ class Header extends Component {
               logout
             </div>
           ) : (
-            <Link to="/login" className="ml1 no-underline black">
+            <Link to='/login' className='ml1 no-underline black'>
               login
             </Link>
           )}
