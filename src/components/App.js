4da29a9d92638ff8a51808538f43229430931d9e
aramirezdecruz3148 <leigh3148@gmail.com>
2019-12-09 10:15:44 -0800

    created new search component and createda route for it

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,18 @@ import Header from './Header';
 import LinkList from './LinkList';
 import Login from './Login';
 import { Switch, Route } from 'react-router-dom';
+import Search from './Search';
 
 function App() {
   return (
-    <div className="center w85">
+    <div className='center w85'>
     <Header />
-    <div className="ph3 pv1 background-gray">
+    <div className='ph3 pv1 background-gray'>
       <Switch>
-        <Route exact path="/" component={LinkList} />
-        <Route exact path="/create" component={CreateLink} />
-        <Route exact path="/login" component={Login} />
+        <Route exact path='/' component={LinkList} />
+        <Route exact path='/create' component={CreateLink} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/search' component={Search} />
       </Switch>
     </div>
   </div>
